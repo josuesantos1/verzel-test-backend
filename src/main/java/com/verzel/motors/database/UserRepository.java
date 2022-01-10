8b71f6fcfd8195f9b7d11a4e8e392df496a1e897
josuesantos1 <josuecsantos2002@gmail.com>
2022-01-10 16:36:35 -0300

    feat(database/userRepository): create UserRepository

---
+++ src/main/java/com/verzel/motors/database/UserRepository.java
@@ -0,0 +1,9 @@
+package com.verzel.motors.database;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserModel, Long> {
+    public Optional<UserModel> findByEmail(String email);
+}
