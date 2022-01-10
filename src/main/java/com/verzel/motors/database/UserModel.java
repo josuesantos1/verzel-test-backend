9db625e02637b47bf3d1c69d8b7e241d33af3ab8
josuesantos1 <josuecsantos2002@gmail.com>
2022-01-10 16:35:18 -0300

    feat(database/UserModel): create userModel

---
+++ src/main/java/com/verzel/motors/database/UserModel.java
@@ -0,0 +1,22 @@
+package com.verzel.motors.database;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "Users")
+public class UserModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+}
