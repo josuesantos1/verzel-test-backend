795cd5b8b38e8b6ba30290edb26b5b2361fafb48
josuesantos1 <josuecsantos2002@gmail.com>
2022-01-12 11:16:42 -0300

    feat(database/carsRepository): carsRepository

---
+++ src/main/java/com/verzel/motors/database/CarsRepository.java
@@ -0,0 +1,10 @@
+package com.verzel.motors.database;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CarsRepository extends JpaRepository<CarsModel, Long> {
+    public Optional<CarsModel> findByName(String name);
+}
