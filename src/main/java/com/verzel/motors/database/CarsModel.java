0bfbd164ac645e352d3b3999b98b10d883de7a2c
josuesantos1 <josuecsantos2002@gmail.com>
2022-01-12 11:15:55 -0300

    feat(database/carsModel): cars model

---
+++ src/main/java/com/verzel/motors/database/CarsModel.java
@@ -0,0 +1,28 @@
+package com.verzel.motors.database;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "Cars")
+public class CarsModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String brand;
+    private String model;
+    private String image;
+    private int year;
+    private String price;
+    private String description;
+}
