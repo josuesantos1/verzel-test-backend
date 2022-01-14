78e0f44af868c657f3defbea26cb0cd3f74063ec
josuesantos1 <josuecsantos2002@gmail.com>
2022-01-14 18:58:36 -0300

    chore(controllers/user): validation email in create user

--- src/main/java/com/verzel/motors/controllers/User.java
+++ src/main/java/com/verzel/motors/controllers/User.java
@@ -2,40 +2,50 @@ package com.verzel.motors.controllers;
 
 import com.verzel.motors.database.UserModel;
 import com.verzel.motors.database.UserRepository;
+import com.verzel.motors.database.Validators.EmailValidator;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import javax.validation.Valid;
 import java.util.Optional;
-
-import static org.springframework.http.ResponseEntity.*;
 
 @RestController
 @RequestMapping("/users")
 public class User {
     private final UserRepository userRepository;
+    private final EmailValidator emailValidator;
     private final PasswordEncoder encoder;
 
-    public User(UserRepository userRepository, PasswordEncoder encoder) {
+    public User(UserRepository userRepository, EmailValidator emailValidator, PasswordEncoder encoder) {
         this.userRepository = userRepository;
+        this.emailValidator = emailValidator;
         this.encoder = encoder;
     }
 
-    @PostMapping("/")
-    public ResponseEntity<UserModel> create(@RequestBody UserModel user) {
+    @PostMapping()
+    public Object create(@RequestBody @Valid UserModel user) {
         String userEmail = user.getEmail();
+        String password = user.getPassword();
 
-        return (ResponseEntity<UserModel>) badRequest();
+        boolean isValid = emailValidator.isValid(userEmail);
+
+        if (!isValid) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("already exists");
+        }
+
+        user.setPassword(encoder.encode(user.getPassword()));
+
+        return ResponseEntity.ok(userRepository.save(user));
     }
 
-    @GetMapping("/")
+    @GetMapping()
     public Object view(@RequestParam String email, @RequestParam String password) {
         Optional<UserModel> user = userRepository.findByEmail(email);
 
         if(user.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("not foud email");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("not found email");
         }
 
         boolean valid = false;
