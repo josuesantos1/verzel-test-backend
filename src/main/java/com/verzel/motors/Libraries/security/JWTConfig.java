246c529811d10428ac0d34729163938f55671a55
josuesantos1 <josuecsantos2002@gmail.com>
2022-01-14 10:04:02 -0300

    feat(libraries/security/JWTConfig): JWTConfig library

---
+++ src/main/java/com/verzel/motors/Libraries/security/JWTConfig.java
@@ -0,0 +1,58 @@
+package com.verzel.motors.Libraries.Security;
+
+import com.verzel.motors.Libraries.DetailUserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@EnableWebSecurity
+public class JWTConfig extends WebSecurityConfigurerAdapter {
+    private final DetailUserServiceImpl userService;
+    private final PasswordEncoder passwordEncoder;
+
+    public JWTConfig(DetailUserServiceImpl userService, PasswordEncoder passwordEncoder) {
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .antMatchers("/login").permitAll()
+                .antMatchers(HttpMethod.GET, "/cars").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilter(new JWTAuthFilter(authenticationManager()))
+                .addFilter(new JWTValidateFilter(authenticationManager()))
+                .cors().disable();
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration corsConfig = new CorsConfiguration().applyPermitDefaultValues();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return source;
+    } // while dev
+
+
+}
