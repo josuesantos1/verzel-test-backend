d2d03744059e96c191d9759f49db3aa48d828241
josuesantos1 <josuecsantos2002@gmail.com>
2022-01-14 18:47:02 -0300

    chore(controllers/cars): upload endpoint; findAllByOwner

--- src/main/java/com/verzel/motors/controllers/Cars.java
+++ src/main/java/com/verzel/motors/controllers/Cars.java
@@ -1,19 +1,24 @@
 package com.verzel.motors.controllers;
 
+import com.verzel.motors.Services.S3Service;
 import com.verzel.motors.database.CarsModel;
 import com.verzel.motors.database.CarsRepository;
-import org.springframework.http.RequestEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/cars")
 public class Cars {
     private final CarsRepository carsRepository;
+    private final S3Service s3Service;
 
-    public Cars(CarsRepository carsRepository) {
+    public Cars(CarsRepository carsRepository, S3Service s3Service) {
         this.carsRepository = carsRepository;
+        this.s3Service = s3Service;
     }
 
     @GetMapping()
@@ -21,20 +26,37 @@ public class Cars {
         return carsRepository.findAll();
     }
 
-    @PatchMapping
+    @PatchMapping("/me")
     public CarsModel update(@RequestBody CarsModel cars) {
         return carsRepository.save(cars);
     }
 
-    @DeleteMapping
+    @DeleteMapping("/me")
     public void delete(@RequestParam Long id) {
         carsRepository.deleteById(id);
         return;
     }
 
-    @PostMapping()
+    @PostMapping("/me/upload")
+    public String upload(@RequestPart MultipartFile file) {
+        String originalFilename = file.getOriginalFilename();
+        String filename = "cars/" + UUID.randomUUID() + "-" + originalFilename;
+
+        try {
+            return s3Service.uploadFile(filename, file.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @PostMapping("/me")
     public CarsModel create(@RequestBody CarsModel car) {
         return carsRepository.save(car);
     }
 
+    @GetMapping("/me")
+    public List<CarsModel> listCarsByOwner(@RequestParam String owner) {
+        return carsRepository.findAllByOwner(owner);
+    }
 }
