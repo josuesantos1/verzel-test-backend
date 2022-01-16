3862870c4bafe81ed303a99187869c5db6529f49
josuesantos1 <josuecsantos2002@gmail.com>
2022-01-16 17:32:49 -0300

    feat(database/templates): upload template

---
+++ src/main/java/com/verzel/motors/database/Templates/Upload.java
@@ -0,0 +1,11 @@
+package com.verzel.motors.database.Templates;
+
+public class Upload {
+    public String key;
+    public String filename;
+
+    public Upload(String key, String filename) {
+        this.key = key;
+        this.filename = filename;
+    }
+}
